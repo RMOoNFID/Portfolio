0cb6369444f7ed58a3742d191f13e9d179cc3a9f
RMOoNFID <rahmoun.dif@gmail.com>
2025-01-28 14:09:08 +0100

    update files

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+
+
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
+    const profilePicture = document.querySelector(".about-pic1");
+    const logoTitle = document.querySelector(".logo");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
-
+    profilePicture.classList.toggle("hidden");
+    logoTitle.classList.toggle("hidden");
 }
